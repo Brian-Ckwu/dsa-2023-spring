c22b0edd7527fd984454cfd84ea6343d2c942c63
Brian-Ckwu <sonny40920710@gmail.com>
2023-03-09 22:25:19 +0800

    feat: implement print_repeated_combination for practicing recursion

--- hw0/p2/recursion_print.c
+++ hw0/p2/recursion_print.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 void print_cartesian(int** arrs, const int arr_len, int* arr_lens, int* index, int depth);
+void print_repeated_combination(int* arr, int arr_size, int k);
 
 int main(void) {
   int a[2] = {1, 2};
@@ -9,14 +10,41 @@ int main(void) {
   int c[2] = {1, 2};
   int d[3] = {1, 3};
 
-  int* arrs[] = {a, b, c, d};
-  const int arr_len = sizeof(arrs) / sizeof(int*);
-  int arr_lens[] = {2,4,2,2};
-  int* index = (int*)calloc(arr_len, sizeof(int));
-  print_cartesian(arrs, arr_len, arr_lens, index, 0);
-  free(index);
+  // int* arrs[] = {a, b, c, d};
+  // const int arr_len = sizeof(arrs) / sizeof(int*);
+  // int arr_lens[] = {2,4,2,2};
+  // int* index = (int*)calloc(arr_len, sizeof(int));
+  // print_cartesian(arrs, arr_len, arr_lens, index, 0);
+  // free(index);
+
+  const int arr_size = sizeof(b) / sizeof(int);
+  int k = 3;
+  print_repeated_combination(b, arr_size, k);
 
   return 0;
+}
+
+void _print_repeated(int* arr, int arr_size, int k, int* index, int depth) {
+  if (depth == k) {
+    int i;
+    for (i = 0; i < k; i++) {
+      printf("%d ", arr[index[i]]);
+    }
+    printf("\n");
+  } else {
+    int i;
+    for (i = 0; i < arr_size; i++) {
+      index[depth] = i;
+      _print_repeated(arr, arr_size, k, index, depth + 1);
+    }
+  }
+}
+
+void print_repeated_combination(int* arr, int arr_size, int k) {
+  int* index = (int*)calloc(k, sizeof(int));
+  int initial_depth = 0;
+  _print_repeated(arr, arr_size, k, index, initial_depth);
+  free(index);
 }
 
 void print_cartesian(int** arrs, const int arr_len, int* arr_lens, int* index, int depth) {
